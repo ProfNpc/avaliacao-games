2c1be5dbba2c188cac1664089c7444ebae3f5c4a
Alessandro <professornpc@gmail.com>
2022-10-20 20:14:17 -0300

    Projeto Base

---
+++ src/main/java/com/belval/avaliacaogames/AvaliacaogamesApplication.java
@@ -0,0 +1,13 @@
+package com.belval.avaliacaogames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaogamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaogamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belval/avaliacaogames/AvaliacaogamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belval.avaliacaogames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaogamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
