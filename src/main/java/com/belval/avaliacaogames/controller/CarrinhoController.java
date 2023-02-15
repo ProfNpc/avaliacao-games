7eda7252d8693c4f16e318a8cfc07a3abded5b3b
Abner <abner.sol.pereira@gmail.com>
2023-02-15 17:01:04 -0300

    add tela de carrinho

---
+++ src/main/java/com/belval/avaliacaogames/controller/CarrinhoController.java
@@ -0,0 +1,13 @@
+package com.belval.avaliacaogames.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CarrinhoController {
+
+	@GetMapping("/carrinho")
+	public String carrinho() {
+		return "carrinho/carrinho";
+	}
+}
