03773056fe42f8c587280f62ea5d6ec796b9d8c5
Abner <abner.sol.pereira@gmail.com>
2023-03-08 14:34:01 -0300

    att

--- src/main/java/com/belval/avaliacaogames/entities/Pagamento.java
+++ src/main/java/com/belval/avaliacaogames/entities/Pagamento.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Pagamento implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -20,6 +22,7 @@ public class Pagamento implements Serializable {
 	private Long codPag;
 	private Instant momPag;
 
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Pedido pedido;
