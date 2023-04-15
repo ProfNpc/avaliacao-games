34bbe1727e5282546594d727c3966c544672f3c1
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2023-04-15 20:55:16 -0300

    Retoque na tela de dados do usuário e edit

--- src/main/resources/static/JS/Tela_Perfil.js
+++ src/main/resources/static/JS/Tela_Perfil.js
@@ -1,8 +1,37 @@
 var inputSenha = document.querySelector(".geral-info input");
 var botaoSenha = document.querySelector(".geral-info button");
+var spanCpf = document.querySelector("#cpf");
 
 if (botaoSenha && botaoSenha.innerHTML == "Editar") {
 	botaoSenha.addEventListener("click", () => {
 	    inputSenha.disabled = !inputSenha.disabled;
 	});
 }
+
+// Não consegui dar um jeito de reutilizar essa função do outro script
+// então eu só vou colar ela aqui :()
+function inserirAoModelo(modelo, str, func) {
+	// Faz uma gambiarra para "inserir" a string no modelo
+	let indiceStr = 0;
+	let retorno = "";
+	for (let i = 0; i < modelo.length; i++) {
+		// Se chegou ao final da string passada, pare
+		if (indiceStr == str.length) break;
+		
+		let charModelo = modelo.charAt(i);
+		if (charModelo == "x") {
+			retorno += str.charAt(indiceStr);
+			indiceStr++;
+		} else {
+			retorno += charModelo;
+		}
+	}
+	
+	// Executa a função passada usando a string processada como parâmetro, se ela existe
+	if (func) func(retorno);
+	
+	// Retorna a string processada
+	return retorno;
+}
+
+spanCpf.innerHTML = inserirAoModelo("xxx.xxx.xxx-xx", spanCpf.innerHTML);
