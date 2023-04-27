b1596f31e5287352918897b6db27fa36b794438b
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2023-04-27 10:51:07 -0300

    Carrossel na página de não logado

--- src/main/resources/static/JS/Tela_Inicial.js
+++ src/main/resources/static/JS/Tela_Inicial.js
@@ -4,12 +4,10 @@ $(document).ready(() => {
 		slidesToShow: 3,
 		slidesToScroll: 2,
 	});
-})
-
-$(document).ready(() => {
+	
 	$("#trocas").slick({
 		infinite: false,
 		slidesToShow: 3,
 		slidesToScroll: 2,
 	});
-})
+});
